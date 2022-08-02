835929e276aabaff3ebc37f2577492672778c836
Damjan Pavlica <damjan.pavlica@origo.is>
2022-08-03 01:56:43 +0200

    m

--- mazes/WeaveGrid.js
+++ mazes/WeaveGrid.js
@@ -30,40 +30,44 @@ export default class WeaveGrid extends Grid {
       if (this.under_cells[i]) yield this.under_cells[i]
   }
 
+  draw_under_cell(ctx, cell, cellSize, x, y, inset) {
+    const x1 = x
+    const x4 = x + cellSize
+    const x2 = x1 + inset
+    const x3 = x4 - inset
+
+    const y1 = y
+    const y4 = y + cellSize
+    const y2 = y1 + inset
+    const y3 = y4 - inset
+
+    if (cell.vertical_passage) {
+      ctx.moveTo(x2, y1)
+      ctx.lineTo(x2, y2)
+      ctx.moveTo(x3, y1)
+      ctx.lineTo(x3, y2)
+      ctx.moveTo(x2, y3)
+      ctx.lineTo(x2, y4)
+      ctx.moveTo(x3, y3)
+      ctx.lineTo(x3, y4)
+      ctx.stroke()
+    } else {
+      ctx.moveTo(x1, y2)
+      ctx.lineTo(x2, y2)
+      ctx.moveTo(x1, y3)
+      ctx.lineTo(x2, y3)
+      ctx.moveTo(x3, y2)
+      ctx.lineTo(x4, y2)
+      ctx.moveTo(x3, y3)
+      ctx.lineTo(x4, y3)
+      ctx.stroke()
+    }
+  }
+
   draw_cell(ctx, cell, cellSize, x, y, inset) {
-    if (cell.constructor.name == 'UnderCell') {
-      const x1 = x
-      const x4 = x + cellSize
-      const x2 = x1 + inset
-      const x3 = x4 - inset
-
-      const y1 = y
-      const y4 = y + cellSize
-      const y2 = y1 + inset
-      const y3 = y4 - inset
-
-      if (cell.vertical_passage) {
-        ctx.moveTo(x2, y1)
-        ctx.lineTo(x2, y2)
-        ctx.moveTo(x3, y1)
-        ctx.lineTo(x3, y2)
-        ctx.moveTo(x2, y3)
-        ctx.lineTo(x2, y4)
-        ctx.moveTo(x3, y3)
-        ctx.lineTo(x3, y4)
-        ctx.stroke()
-      } else {
-        ctx.moveTo(x1, y2)
-        ctx.lineTo(x2, y2)
-        ctx.moveTo(x1, y3)
-        ctx.lineTo(x2, y3)
-        ctx.moveTo(x3, y2)
-        ctx.lineTo(x4, y2)
-        ctx.moveTo(x3, y3)
-        ctx.lineTo(x4, y3)
-        ctx.stroke()
-      }
-    } else
+    if (cell.constructor.name == 'UnderCell')
+      this.draw_under_cell(ctx, cell, cellSize, x, y, inset)
+    else
       super.draw_cell(ctx, cell, cellSize, x, y, inset)
   }
 }
