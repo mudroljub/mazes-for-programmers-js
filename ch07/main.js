86b174a513dad7e452274be872eb3790b0880bf0
Damjan Pavlica <damjan.pavlica@origo.is>
2022-08-04 00:08:27 +0200

    new

---
+++ ch07/main.js
@@ -0,0 +1,16 @@
+import RecursiveBacktracker from './RecursiveBacktracker.js'
+// import RecursiveBacktracker from '../mazes/algorithms/RecursiveBacktracker.js'
+import PolarGrid from './PolarGrid.js'
+
+const rows = 8
+const grid = new PolarGrid(rows)
+const cellSize = 20
+const img_size = 2 * rows * cellSize
+
+RecursiveBacktracker.on(grid)
+
+const output = document.getElementById('output')
+output.width = img_size
+output.height = img_size
+const ctx = output.getContext('2d')
+grid.to_img(ctx, cellSize)
